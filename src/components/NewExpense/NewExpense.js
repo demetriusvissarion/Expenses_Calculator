ccd97309a0f8f9a879df65344c8609eb63919562
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2022-12-22 10:28:10 +0000

    child to parent communication

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,12 +3,25 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDatahandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData />
+      <ExpenseForm onSaveExpenseData={saveExpenseDatahandler} />
     </div>
   );
 };
 
 export default NewExpense;
+
+/* 
+here onSaveExpenseData is a function
+onSaveExpenseData prop receives saveExpenseDatahandler function as a value, so we don't execute it here, we just point at the function, so that hte function itself is passed to expense form
+*/
