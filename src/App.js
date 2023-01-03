853fd3f19803b8f0e32c34e3f51f47a8f294adbb
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-03 16:03:57 +0000

    Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ import Expenses from "./components/Expenses/Expenses";
 import Child1 from "./TestCounter/child1";
 import "./TestCounter/child1.css";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 4.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 4.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // setExpenses([expense, ...expenses]);  // this would work but it's not good practice
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]; // this is good practice, because based on older snapshot
+    });
   };
 
   // return React.createElement(
